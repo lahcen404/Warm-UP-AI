24c6c556b9dda05a3b81bb30e532006ae0f15d89
lahcen404 <lahcen.maskour2003@gmail.com>
2026-09-03 09:50:22 +0100

    listes avance

--- Bases-Python/main.py
+++ Bases-Python/main.py
@@ -1,3 +1,17 @@
 ## Challenge 1
 
-name = input("Enter ur name ")
+
+
+
+#  Écrivez du code Python ici
+
+liste1 = [1, 3, 5, 3]
+liste2 = [2, 3, 4, 5]
+
+liste1.extend(liste2)
+
+sans_double = set(liste1)
+
+sorteed = sorted(sans_double)
+
+print(sorteed)
